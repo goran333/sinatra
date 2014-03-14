665280a316f358a4a749458262e86dbcb5607600
goran333 <goran333@gmail.com>
2014-03-13 22:26:10 -0600

    highchart example finally works

--- highcharts/main.rb
+++ highcharts/main.rb
@@ -18,7 +18,6 @@ def highchart_example
     f.series(:type=> 'column',:name=> 'Jane',:data=> [3, 2, 1, 3, 4])
     f.series(:type=> 'column',:name=> 'John',:data=> [2, 3, 5, 7, 6])
     f.series(:type=> 'column', :name=> 'Joe',:data=> [4, 3, 3, 9, 0])
-    f.series(:type=> 'column', :name=> 'Joe',:data=> [4, 3, 3, 9, 0])
     f.series(:type=> 'spline',:name=> 'Average', :data=> [3, 2.67, 3, 6.33, 3.33])
     f.series(:type=> 'pie',:name=> 'Total consumption', 
              :data=> [
